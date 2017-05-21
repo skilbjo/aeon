9deb1c1b412131dc7b8f482086fbf5758de21bb7
John Skilbeck <john.skilbeck@gmail.com>
2017-05-20 23:50:44 -0700

    memoize db call

--- src/controller.clj
+++ src/controller.clj
@@ -24,7 +24,9 @@
                                              from dw.:table
                                              order by date desc
                                              limit 10")
-        rs          (sql/query sql {:table dataset})
-        transformed (->> rs
+        rs          (memoize (fn []
+                               (sql/query sql
+                                          {:table dataset})))
+        transformed (->> (rs)
                          (util/map-seq-fkv-v util/date-me))]
     {:body transformed}))
