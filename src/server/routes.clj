976e43b4ce00b437635f6840e403b130cdb0e184
John Skilbeck <john.skilbeck@gmail.com>
2017-11-18 00:03:49 -0800

    Better headers

--- src/server/routes.clj
+++ src/server/routes.clj
@@ -5,9 +5,9 @@
             [jobs.static :as jobs.static]
             [jobs.api :as jobs.api]
             [ring.adapter.jetty :as jetty]
+            [ring.middleware.anti-forgery :as anti-forgery]
             [ring.middleware.defaults :as ring-defaults]
             [ring.middleware.json :as ring-json]
-            [ring.middleware.anti-forgery :as anti-forgery]
             [ring.middleware.session :as session]
             [ring.util.response :refer [response]])
   (:gen-class))
@@ -27,7 +27,12 @@
 
 (defroutes combined-routes
   (-> site-routes
-      (ring-defaults/wrap-defaults ring-defaults/site-defaults))
+      (ring-defaults/wrap-defaults (assoc ring-defaults/site-defaults
+                                          :security {:content-type-options :nosniff
+                                                     :anti-forgery   true
+                                                     :hsts           true
+                                                     :frame-options  :sameorigin
+                                                     :xss-protection {:enable? true :mode :block}})))
   (-> api-routes
       (ring-json/wrap-json-response))
   (route/not-found "<h1>Not Found</h1>"))
