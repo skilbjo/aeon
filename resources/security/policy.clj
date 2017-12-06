cbc43d08bcf1a4485f5461fed23e8c9f9d5ebcb2
John Skilbeck <john.skilbeck@gmail.com>
2017-12-05 23:34:48 -0800

    domains on security policy

--- resources/security/policy.clj
+++ resources/security/policy.clj
@@ -3,19 +3,22 @@
                       "https://skilbjo-sfo.duckdns.org"
                       "https://skilbjo-aws.duckdns.org"
                       "https://skilbjo-robot.duckdns.org"
+                      "https://julianna-and-john.duckdns.org"
+                      "http://localhost:8080"
                       :https
-                      :unsafe-inline
-                      ]
+                      :unsafe-inline]
            :style    ["https://skilbjo.duckdns.org"
                       "https://skilbjo-sfo.duckdns.org"
                       "https://skilbjo-aws.duckdns.org"
                       "https://skilbjo-robot.duckdns.org"
+                      "https://julianna-and-john.duckdns.org"
                       "http://localhost:8080"]
            :img      "*"
            :object   :none
            :connect  ["http://skilbjo.duckdns.org"
+                      "http://skilbjo-sfo.duckdns.org"
                       "http://skilbjo-aws.duckdns.org"
                       "http://skilbjo-robot.duckdns.org"
+                      "https://julianna-and-john.duckdns.org"
                       "http://localhost:8080"]
-           :child    :self
-           }}
+           :child    :self}}
