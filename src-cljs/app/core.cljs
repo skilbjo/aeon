5178238c3b264673efd71a3d51c5a089bbdd8f01
John Skilbeck <john.skilbeck@gmail.com>
2018-03-20 23:56:30 -0700

    Working now, pretty cool

--- src-cljs/app/core.cljs
+++ src-cljs/app/core.cljs
@@ -3,24 +3,27 @@
             [reagent.core :as reagent]
             [re-frame.core :as re-frame]))
 
-(enable-console-print!)
-
-(defn main []
-  (println "fuck"))
-
 (defonce app-state (atom {:text "Hello world!"}))
+
+(enable-console-print!)
 
 (defn main-component []
   (fn []
     [:div ["hello"]
-     [:h1 "hello sss"]]))
+     [:h1 "hello there"]
+     [:h2 "yo"]
+     [:h3 "more stuff"]
+     [:h4 (* 5 5)]]))
 
 (defn mount-root []
   (reagent/render [main-component] (.getElementById js/document "app")))
 
 (defn init! []
-  (do
-    (mount-root)
-    (println "yoos")))
+  (do (mount-root)
+      (println "yoas")))
 
-(init!)
+(defn main []
+  (init!)
+  (println "fuck"))
+
+(main)
