52af9eedce35525dbd54b64340e2d545cd793ced
John Skilbeck <john.skilbeck@gmail.com>
2018-06-17 19:25:22 -0700

    Upgrade library dependencies, add suggestions from lein deps :tree

--- project.clj
+++ project.clj
@@ -1,36 +1,45 @@
 (defproject compojure "0.1.0"
   :uberjar-name "app.jar"
-  :repositories {"atlassian" {:url "https://maven.atlassian.com/content/repositories/atlassian-3rdparty/"}}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :repositories {"atlassian"
+                 {:url "https://maven.atlassian.com/content/repositories/atlassian-3rdparty/"}}
+  :dependencies [;; clj
+                 [org.clojure/clojure "1.9.0"]
                  [bk/ring-gzip "0.3.0"]
-                 [clj-http "3.7.0"]
-                 [clj-time "0.14.2"]
+                 [clj-http "3.9.0"]
+                 [clj-time "0.14.4"]
                  [com.amazonaws.athena.jdbc/AthenaJDBC41 "1.0.1-atlassian-hosted"]
-                 [compojure "1.5.1" :exclusions [ring/ring-core]]
-                 [de.ubercode.clostache/clostache "1.3.1"]
+                 [compojure "1.6.1" :exclusions [ring/ring-core]]
+                 [de.ubercode.clostache/clostache "1.4.0"]
                  [environ "1.1.0"]
                  [hiccup "1.0.5"]
                  [jarohen/chime "0.2.2"]
-                 [markdown-clj "0.9.99"]
-                 [net.sf.uadetector/uadetector-resources "2013.02"]
+                 [markdown-clj "1.0.2"]
+                 [net.sf.uadetector/uadetector-resources "2014.10"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/java.jdbc "0.7.3"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [org.postgresql/postgresql "42.1.4"]
-                 [org.slf4j/slf4j-log4j12 "1.6.4"]
+                 [org.clojure/java.jdbc "0.7.6"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/tools.reader "1.2.2"]
+                 [org.postgresql/postgresql "42.2.2"]
+                 [org.slf4j/slf4j-log4j12 "1.7.25"
+                  :exclusions [org.clojure/tools.reader
+                               org.slf4j/slf4j-api]]
                  [ring "1.6.3"]
-                 [ring/ring-anti-forgery "1.1.0"]
-                 [ring/ring-defaults "0.3.1" ]
+                 [ring/ring-anti-forgery "1.3.0"]
+                 [ring/ring-defaults "0.3.2" ]
                  [ring/ring-json "0.4.0"]
-                 [venantius/ultra "0.5.1" :exclusions [instaparse]]
-                 ; cljs
-                 [org.clojure/clojurescript "1.9.946"]
-                 [binaryage/devtools "0.9.9"]
-                 [cljsjs/jquery "2.1.4-0"]
-                 [cljsjs/react "15.6.2-4"]
-                 [cljsjs/react-dom "15.6.2-4"]
-                 [re-frame "0.10.5"]
-                 [reagent "0.7.0"]]
+
+                 ;; cljs
+                 [org.clojure/clojurescript "1.10.312"
+                  :exclusions [com.google.code.findbugs/jsr305
+                               com.fasterxml.jackson.core/jackson-core
+                               org.clojure/tools.reader]]
+                 [binaryage/devtools "0.9.10"]
+                 [cljsjs/jquery "3.2.1-0"]
+                 [cljsjs/react "16.4.0-0"]
+                 [cljsjs/react-dom "16.4.0-0"]
+                 [re-frame "0.10.5"
+                  :exclusions [com.google.code.findbugs/jsr305]]
+                 [reagent "0.8.1"]]
   :plugins [[lein-cloverage "1.0.10"]
             [lein-cljsbuild "1.1.7"]]
   :source-paths ["src"]
@@ -42,10 +51,10 @@
                                         :output-dir    "resources/public/js/out"
                                         :output-to     "resources/public/js/app.js"}}}}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-mock "0.3.0"]]
+                                  [ring/ring-mock "0.3.2" :exclusions [cheshire]]]
                    :plugins [[lein-cljfmt "0.5.7"]
                              [lein-environ "1.1.0"]
-                             [lein-figwheel "0.5.14"]
+                             [lein-figwheel "0.5.16"]
                              [lein-ring "0.12.0"]]
                    :figwheel {:css-dirs ["resources/public/css"]
                               :ring-handler server.routes/app
