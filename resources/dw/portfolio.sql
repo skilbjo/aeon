6c15f88bc4aa6a228abf087c138cde0036a7a613
John Skilbeck <john.skilbeck@gmail.com>
2018-09-21 00:27:16 -0700

    40->50

--- resources/dw/portfolio.sql
+++ resources/dw/portfolio.sql
@@ -13,7 +13,7 @@ with now as (
       else        (select now from now) - 1
     end as yesterday
 ), max_known_date as (
-  select max(date) max_known_date from ( select date, count(*) from dw.equities_fact group by date having count(*) > 40) src
+  select max(date) max_known_date from ( select date, count(*) from dw.equities_fact group by date having count(*) > 50) src
 ), beginning_of_year as (
   select date_trunc('year', ( select now from now)) + interval '1 day' beginning_of_year
 ), equities as (
