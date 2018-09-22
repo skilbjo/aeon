c4400e5dbb6d3eac1fd4a297147090c028206c1b
John Skilbeck <john.skilbeck@gmail.com>
2018-09-22 02:28:42 -0700

    remove %

--- resources/athena/portfolio_athena.sql
+++ resources/athena/portfolio_athena.sql
@@ -150,11 +150,11 @@ with now_ts as (
     cast(cost_basis as integer) cost_basis,
     cast(market_value as integer) market_value,
     cast(today_gain_loss as integer) today_gain_loss,
-    cast(cast((today_gain_loss / market_value * 100) as decimal(8,2)) as varchar) || '%'  "today_gain_loss_%",
+    cast(cast((today_gain_loss / market_value * 100) as decimal(8,2)) as varchar) "today_gain_loss_%",
     cast(ytd_gain_loss as integer) today_gain_loss,
-    cast(cast((ytd_gain_loss / market_value * 100) as decimal(8,2)) as varchar) || '%'  "ytd_gain_loss_%",
+    cast(cast((ytd_gain_loss / market_value * 100) as decimal(8,2)) as varchar)   "ytd_gain_loss_%",
     cast(gain_loss as integer) total_gain_loss,
-    cast(cast((gain_loss / cost_basis * 100) as decimal(8,2)) as varchar) || '%'  "total_gain_loss_%"
+    cast(cast((gain_loss / cost_basis * 100) as decimal(8,2)) as varchar)         "total_gain_loss_%"
   from
     _union
 )
