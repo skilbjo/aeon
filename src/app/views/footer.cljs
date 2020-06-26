36b9b2345ade945da57692b717f614ad153a71df
John Skilbeck <john.skilbeck@gmail.com>
2020-06-25 18:53:56 -0700

    Add statuspage to footer

--- src/app/views/footer.cljs
+++ src/app/views/footer.cljs
@@ -1,17 +1,28 @@
 (ns app.views.footer
   (:require [re-frame.core :refer [subscribe dispatch]]))
 
+(def source-code
+  "https://github.com/skilbjo/aeon")
+
 (def swagger-uri
   (str "https://skilbjo.duckdns.org" "/swagger"))
+
+(def statuspage
+  "https://skilbjo.statuspage.io/")
 
 (defn footer []
   [:footer.page-footer
    [:div.container
     [:div.row]]
    [:div.footer-copyright
-    [:div.container
-     [:a.grey-text.text-lighten-4.left
-      {:href "https://github.com/skilbjo/aeon"}
-      "source code"]]
-    [:div.container
-     [:a.grey-text.text-lighten-4.right {:href swagger-uri} "swagger api"]]]])
+     [:div.container
+      [:a.grey-text.text-lighten-4.left
+       {:href source-code} "source code"]
+      [:div.container
+       [:div.container
+        [:a.grey-text.text-lighten-4.left
+         {:href statuspage} "status"]]
+       [:div.container
+        [:div.container
+         [:a.grey-text.text-lighten-4.left
+          {:href swagger-uri} "swagger api"]]]]]]])
